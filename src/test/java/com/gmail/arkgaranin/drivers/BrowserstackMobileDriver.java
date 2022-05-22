011a2065ed386c7dda9ce6dab04c0588dff878f1
Arkadiy <arkadiy.garanin@socialsys.ru>
2022-05-22 21:46:27 +0300

    Отредактировал класс BrowserstackMobileDriver

--- src/test/java/com/gmail/arkgaranin/drivers/BrowserstackMobileDriver.java
+++ src/test/java/com/gmail/arkgaranin/drivers/BrowserstackMobileDriver.java
@@ -1,21 +1,27 @@
 package com.gmail.arkgaranin.drivers;
 
 import com.codeborne.selenide.WebDriverProvider;
+import com.gmail.arkgaranin.config.CredentialsConfig;
+import io.appium.java_client.android.AndroidDriver;
+import org.aeonbits.owner.ConfigFactory;
 import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.MutableCapabilities;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.net.MalformedURLException;
 import java.net.URL;
 
-import static com.gmail.arkgaranin.helpers.Browserstack.*;
-
 public class BrowserstackMobileDriver implements WebDriverProvider {
+
+  public static CredentialsConfig config = ConfigFactory.create(CredentialsConfig.class);
+
+  String user = config.browserStackUser();
+  String key = config.browserStackKey();
+  String url = config.browserStackAppURL();
 
   public static URL getBrowserstackUrl() {
     try {
-      return new URL(browserstackUrl);
+      return new URL("http://hub.browserstack.com/wd/hub");
     } catch (MalformedURLException e) {
       throw new RuntimeException(e);
     }
@@ -25,16 +31,15 @@ public class BrowserstackMobileDriver implements WebDriverProvider {
   public WebDriver createDriver(Capabilities capabilities) {
     MutableCapabilities mutableCapabilities = new MutableCapabilities();
     mutableCapabilities.merge(capabilities);
-    mutableCapabilities.setCapability("browserstack.appium_version", "1.22.0");
-    mutableCapabilities.setCapability("browserstack.user", browserstackLogin);
-    mutableCapabilities.setCapability("browserstack.key", browserstackPassword);
-    mutableCapabilities.setCapability("app", appUrl);
+    mutableCapabilities.setCapability("browserstack.user", user);
+    mutableCapabilities.setCapability("browserstack.key", key);
+    mutableCapabilities.setCapability("app", url);
     mutableCapabilities.setCapability("device", "Google Pixel 3");
     mutableCapabilities.setCapability("os_version", "9.0");
     mutableCapabilities.setCapability("project", "First Java Project");
     mutableCapabilities.setCapability("build", "browserstack-build-1");
     mutableCapabilities.setCapability("name", "first_test");
 
-    return new RemoteWebDriver(getBrowserstackUrl(), mutableCapabilities);
+    return new AndroidDriver(getBrowserstackUrl(), mutableCapabilities);
   }
 }
