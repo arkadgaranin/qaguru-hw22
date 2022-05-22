cb571d96a00dc2b1ee19e236c2655e7adb115f9e
Arkadiy <arkadiy.garanin@socialsys.ru>
2022-05-22 21:39:52 +0300

    Добавил методы для получения url-а видео

--- src/test/java/com/gmail/arkgaranin/helpers/Attach.java
+++ src/test/java/com/gmail/arkgaranin/helpers/Attach.java
@@ -1,13 +1,17 @@
 package com.gmail.arkgaranin.helpers;
 
+import com.codeborne.selenide.Selenide;
 import io.qameta.allure.Attachment;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.nio.charset.StandardCharsets;
 
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
+import static org.openqa.selenium.logging.LogType.BROWSER;
 
 public class Attach {
   @Attachment(value = "{attachName}", type = "text/plain")
@@ -32,7 +36,34 @@ public class Attach {
         + "' type='video/mp4'></video></body></html>";
   }
 
+  private static String getVideoUrl(String sessionId) {
+    String deviceFarm = System.getProperty("deviceFarm");
+
+    if (deviceFarm.equals("browserstack")) {
+      return Browserstack.videoUrl(sessionId);
+    } else if (deviceFarm.equals("selenoid")) {
+      return getSelenoidVideoUrl(sessionId);
+    }
+    return null;
+  }
+
+  public static String getSelenoidVideoUrl(String sessionId) {
+    try {
+      return new URL("https://selenoid.autotests.cloud/video/" + sessionId + ".mp4") + "";
+    } catch (MalformedURLException e) {
+      e.printStackTrace();
+    }
+    return null;
+  }
+
   public static String getSessionId() {
     return ((RemoteWebDriver) getWebDriver()).getSessionId().toString();
+  }
+
+  public static void browserConsoleLogs() {
+    attachAsText(
+        "Browser console logs",
+        String.join("\n", Selenide.getWebDriverLogs(BROWSER))
+    );
   }
 }
